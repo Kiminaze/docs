c2a846753175c1d8ab09b38db6b0fd332fa558a4
Kiminaze <kiminaze@yahoo.de>
2025-10-09 12:17:19 +0200

    Fix yt placeholder for multiple videos

--- docs/javascripts/yt_replace.js
+++ docs/javascripts/yt_replace.js
@@ -1,35 +1,32 @@
-
+﻿
 document.addEventListener("DOMContentLoaded", function () {
     var consent = __md_get("__consent");
 
-    if (consent) {
-        if (consent.youtube) {
-            var placeholder = document.getElementById("youtube-placeholder");
-
-            if (placeholder) {
-                placeholder.innerHTML = (`
+    const placeholders = document.getElementsByClassName("youtube-placeholder");
+    for (const ph of placeholders) {
+        if (ph) {
+            if (consent && consent.youtube) {
+                ph.innerHTML = (`
                     <iframe 
                         width="560" 
                         height="315" 
                         src="https://www.youtube-nocookie.com/embed/VIDEO_ID" 
-                        title="YouTube video player" 
+                        title="VIDEO_TITLE" 
                         frameborder="0" 
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
                         referrerpolicy="strict-origin-when-cross-origin" 
                         allowfullscreen>
                     </iframe>
-                `).replace("VIDEO_ID", placeholder.dataset.videoid);
+                `).replace("VIDEO_ID", ph.dataset.videoid).replace("VIDEO_TITLE", ph.dataset.videotitle);
             }
-        }
-        else {
-            var placeholder = document.getElementById("youtube-placeholder");
-
-            if (placeholder) {
-                placeholder.innerHTML = (`
-                    <a href="https://www.youtube.com/watch?v=VIDEO_ID" target="_blank">
-                        <img src="https://img.youtube.com/vi/VIDEO_ID/hqdefault.jpg">
-                    </a>
-                `).replaceAll("VIDEO_ID", placeholder.dataset.videoid);
+            else {
+                ph.innerHTML = (`
+                    <div class="video-link">
+                        <a href="https://www.youtube.com/watch?v=VIDEO_ID" target="_blank" rel="noopener">
+                        ▶ Watch VIDEO_TITLE on YouTube
+                        </a>
+                    </div>
+                `).replace("VIDEO_ID", ph.dataset.videoid).replace("VIDEO_TITLE", ph.dataset.videotitle);
             }
         }
     }
